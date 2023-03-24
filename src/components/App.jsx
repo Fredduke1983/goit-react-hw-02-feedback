f31c4c109d72071bf533aafebd0727f885ff6091
Nechai_Ihor <fredduke1983@gmail.com>
2023-03-24 21:44:16 +0200

    delete Feedback add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,49 @@
-import { Feedback } from './Feedback/Feedback';
+import React, { Component } from 'react';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Notification } from 'components/Notification/Notification';
+import { FeedbackStyle, StatTitle } from './app.styled';
 
-export const App = () => {
-  return <Feedback />;
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onBtnClick = e => {
+    const { name } = e.currentTarget;
+    this.setState(({ good, neutral, bad }) => {
+      if (name === 'good') {
+        return {
+          good: good + 1,
+        };
+      } else if (name === 'neutral') {
+        return {
+          neutral: neutral + 1,
+        };
+      } else if (name === 'bad') {
+        return {
+          bad: bad + 1,
+        };
+      }
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return (
+      <FeedbackStyle>
+        <FeedbackOptions onLeaveFeedback={this.onBtnClick} />
+
+        <StatTitle>Statistic</StatTitle>
+        {total ? (
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        ) : (
+          <Notification />
+        )}
+      </FeedbackStyle>
+    );
+  }
+}
